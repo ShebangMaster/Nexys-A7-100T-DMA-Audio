159b1f9de17746706820c01ec9fe5189bd3c5fb3
MW_Engineering <106823236+ShebangMaster@users.noreply.github.com>
2025-05-15 21:37:00 +0300

    Add files via upload

---
+++ design_1_wrapper.v
@@ -0,0 +1,112 @@
+//Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
+//Date        : Fri Nov  6 14:29:25 2020
+//Host        : K-Legion running 64-bit Kali GNU/Linux Rolling
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (PWM_AUDIO_0_en,
+    PWM_AUDIO_0_pwm,
+    ddr2_sdram_addr,
+    ddr2_sdram_ba,
+    ddr2_sdram_cas_n,
+    ddr2_sdram_ck_n,
+    ddr2_sdram_ck_p,
+    ddr2_sdram_cke,
+    ddr2_sdram_cs_n,
+    ddr2_sdram_dm,
+    ddr2_sdram_dq,
+    ddr2_sdram_dqs_n,
+    ddr2_sdram_dqs_p,
+    ddr2_sdram_odt,
+    ddr2_sdram_ras_n,
+    ddr2_sdram_we_n,
+    dip_switches_16bits_tri_i,
+    led_16bits_tri_o,
+    push_buttons_5bits_tri_i,
+    reset,
+    rgb_led_tri_o,
+    sys_clock,
+    usb_uart_rxd,
+    usb_uart_txd);
+  output PWM_AUDIO_0_en;
+  output PWM_AUDIO_0_pwm;
+  output [12:0]ddr2_sdram_addr;
+  output [2:0]ddr2_sdram_ba;
+  output ddr2_sdram_cas_n;
+  output [0:0]ddr2_sdram_ck_n;
+  output [0:0]ddr2_sdram_ck_p;
+  output [0:0]ddr2_sdram_cke;
+  output [0:0]ddr2_sdram_cs_n;
+  output [1:0]ddr2_sdram_dm;
+  inout [15:0]ddr2_sdram_dq;
+  inout [1:0]ddr2_sdram_dqs_n;
+  inout [1:0]ddr2_sdram_dqs_p;
+  output [0:0]ddr2_sdram_odt;
+  output ddr2_sdram_ras_n;
+  output ddr2_sdram_we_n;
+  input [15:0]dip_switches_16bits_tri_i;
+  output [15:0]led_16bits_tri_o;
+  input [4:0]push_buttons_5bits_tri_i;
+  input reset;
+  output [5:0]rgb_led_tri_o;
+  input sys_clock;
+  input usb_uart_rxd;
+  output usb_uart_txd;
+
+  wire PWM_AUDIO_0_en;
+  wire PWM_AUDIO_0_pwm;
+  wire [12:0]ddr2_sdram_addr;
+  wire [2:0]ddr2_sdram_ba;
+  wire ddr2_sdram_cas_n;
+  wire [0:0]ddr2_sdram_ck_n;
+  wire [0:0]ddr2_sdram_ck_p;
+  wire [0:0]ddr2_sdram_cke;
+  wire [0:0]ddr2_sdram_cs_n;
+  wire [1:0]ddr2_sdram_dm;
+  wire [15:0]ddr2_sdram_dq;
+  wire [1:0]ddr2_sdram_dqs_n;
+  wire [1:0]ddr2_sdram_dqs_p;
+  wire [0:0]ddr2_sdram_odt;
+  wire ddr2_sdram_ras_n;
+  wire ddr2_sdram_we_n;
+  wire [15:0]dip_switches_16bits_tri_i;
+  wire [15:0]led_16bits_tri_o;
+  wire [4:0]push_buttons_5bits_tri_i;
+  wire reset;
+  wire [5:0]rgb_led_tri_o;
+  wire sys_clock;
+  wire usb_uart_rxd;
+  wire usb_uart_txd;
+
+  design_1 design_1_i
+       (.PWM_AUDIO_0_en(PWM_AUDIO_0_en),
+        .PWM_AUDIO_0_pwm(PWM_AUDIO_0_pwm),
+        .ddr2_sdram_addr(ddr2_sdram_addr),
+        .ddr2_sdram_ba(ddr2_sdram_ba),
+        .ddr2_sdram_cas_n(ddr2_sdram_cas_n),
+        .ddr2_sdram_ck_n(ddr2_sdram_ck_n),
+        .ddr2_sdram_ck_p(ddr2_sdram_ck_p),
+        .ddr2_sdram_cke(ddr2_sdram_cke),
+        .ddr2_sdram_cs_n(ddr2_sdram_cs_n),
+        .ddr2_sdram_dm(ddr2_sdram_dm),
+        .ddr2_sdram_dq(ddr2_sdram_dq),
+        .ddr2_sdram_dqs_n(ddr2_sdram_dqs_n),
+        .ddr2_sdram_dqs_p(ddr2_sdram_dqs_p),
+        .ddr2_sdram_odt(ddr2_sdram_odt),
+        .ddr2_sdram_ras_n(ddr2_sdram_ras_n),
+        .ddr2_sdram_we_n(ddr2_sdram_we_n),
+        .dip_switches_16bits_tri_i(dip_switches_16bits_tri_i),
+        .led_16bits_tri_o(led_16bits_tri_o),
+        .push_buttons_5bits_tri_i(push_buttons_5bits_tri_i),
+        .reset(reset),
+        .rgb_led_tri_o(rgb_led_tri_o),
+        .sys_clock(sys_clock),
+        .usb_uart_rxd(usb_uart_rxd),
+        .usb_uart_txd(usb_uart_txd));
+endmodule
